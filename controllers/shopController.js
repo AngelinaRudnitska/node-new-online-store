6d4d5209c6f9da40ee15aa75d26369d7e9f4e0db
Angelina <angelinarudnitska2312@gmail.com>
2021-05-10 03:02:58 +0300

    update

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,52 +1,51 @@
+const Product = require("../models/product")
 
-exports.getBlogPage = (req, res, next) => {
-    res.render("pages/blog")
-}
 
-exports.getCartPage = (req, res, next) => {
-    res.render("pages/cart")
-}
-
-exports.getCategoryPage = (req, res, next) => {
-    res.render("pages/category")
-}
-
-exports.getCheckoutPage = (req, res, next) => {
-    res.render("pages/checkout")
-}
-
-exports.getConfirmationPage = (req, res, next) => {
-    res.render("pages/confirmation")
-}
-
-exports.getContactPage = (req, res, next) => {
-    res.render("pages/contact")
-}
 
 exports.getHomePage = (req, res, next) => {
-    res.render("pages/index")
+    res.render('pages/index');
 }
-
-exports.getLoginPage = (req, res, next) => {
-    res.render("pages/login")
+exports.getBlogPage = (req, res, next) => {
+    res.render('pages/blog');
 }
-
-exports.getRegisterPage = (req, res, next) => {
-    res.render("pages/register")
+exports.getContactPage = (req, res, next) => {
+    res.render('pages/contact');
 }
-
 exports.getSingleBlogPage = (req, res, next) => {
-    res.render("pages/single-blog")
+    res.render('pages/single-blog');
 }
-
+exports.getTrackingPage = (req, res, next) => {
+    res.render('pages/tracking');
+}
+exports.getConfirmationPage = (req, res, next) => {
+    res.render('pages/confirmation');
+}
+exports.getLoginPage = (req, res, next) => {
+    res.render('pages/login');
+}
+exports.getCheckoutPage = (req, res, next) => {
+    res.render('pages/product-checkout');
+}
 exports.getSingleProductPage = (req, res, next) => {
-    res.render("pages/single-product")
+    res.render('pages/single-product');
+}
+exports.getRegisterPage = (req, res, next) => {
+    res.render('pages/register');
+}
+exports.getShopCategoryPage = (req, res, next) => {
+    Product.findAll()
+        .then((products) => {
+            res.render('pages/shop-category', {
+                products: products,
+            })
+        })
+        .catch(err => console.log(error));
 }
 
-exports.getTrackingOrderPage = (req, res, next) => {
-    res.render("pages/tracking-order")
-}
 
+exports.getShopingCartPage = (req, res, next) => {
+    res.render('pages/shoping-cart');
+}
 exports.get404 = (req, res, next) => {
     res.status(404).render("pages/404.ejs")
 }
